da86ddfb0e15e91dbdfcd1d69c894072cafbf266
Haowei Hsu <hwhsu1231@gmail.com>
2024-03-20 18:03:08 +0800

    Revert "FindGettext: add copyright comments"
    This reverts commit c80cd0ad429108d7778692d97075b52784540edd.

--- FindGettext.cmake
+++ FindGettext.cmake
@@ -1,6 +1,3 @@
-# Distributed under the OSI-approved BSD 3-Clause License.
-# See accompanying file LICENSE.txt for details.
-
 #[=======================================================================[.rst:
 FindGettext
 -----------
