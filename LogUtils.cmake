3fd8dc91d134988992cb0a96da4213bf0d817660
Haowei Hsu <hwhsu1231@gmail.com>
2024-04-12 16:41:11 +0800

    LogUtils: update chore

--- LogUtils.cmake
+++ LogUtils.cmake
@@ -1,23 +1,10 @@
 # Distributed under the OSI-approved BSD 3-Clause License.
 # See accompanying file LICENSE.txt for details.
 
-# macro(remove_cmake_message_indent)
-#     execute_process(COMMAND ${CMAKE_COMMAND} -E sleep 2.0)
-#     set(CMAKE_MESSAGE_INDENT_BACKUP "${CMAKE_MESSAGE_INDENT}")
-#     set(CMAKE_MESSAGE_INDENT "")        
-# endmacro()
-
-# macro(restore_cmake_message_indent)
-#     set(CMAKE_MESSAGE_INDENT "${CMAKE_MESSAGE_INDENT_BACKUP}")
-#     set(CMAKE_MESSAGE_INDENT_BACKUP "")
-#     execute_process(COMMAND ${CMAKE_COMMAND} -E sleep 2.0)
-# endmacro()
-
 macro(remove_cmake_message_indent)
     if(DEFINED REMOVE_CMAKE_MESSAGE_INDENT_CALLED)
         message(FATAL_ERROR "remove_cmake_message_indent() has already been called without calling restore_cmake_message_indent().")
     endif()
-
     execute_process(COMMAND ${CMAKE_COMMAND} -E sleep 2.0)
     set(CMAKE_MESSAGE_INDENT_BACKUP "${CMAKE_MESSAGE_INDENT}")
     set(CMAKE_MESSAGE_INDENT "")        
@@ -28,7 +15,6 @@ macro(restore_cmake_message_indent)
     if(NOT DEFINED REMOVE_CMAKE_MESSAGE_INDENT_CALLED)
         message(FATAL_ERROR "remove_cmake_message_indent() must be called before calling restore_cmake_message_indent().")
     endif()
-
     set(CMAKE_MESSAGE_INDENT "${CMAKE_MESSAGE_INDENT_BACKUP}")
     set(CMAKE_MESSAGE_INDENT_BACKUP "")
     execute_process(COMMAND ${CMAKE_COMMAND} -E sleep 2.0)
