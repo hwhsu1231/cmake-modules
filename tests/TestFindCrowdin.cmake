50135633f9c1821f1a6797c7a5cc90bbf2937e4c
Haowei Hsu <hwhsu1231@gmail.com>
2024-06-13 18:52:39 +0800

    update

--- tests/TestFindCrowdin.cmake
+++ tests/TestFindCrowdin.cmake
@@ -1,3 +1,11 @@
+
+cmake_minimum_required(VERSION 3.23)
 message(STATUS "")
 message(STATUS "---------- TestFindCrowdin ----------")
 message(STATUS "")
+
+set(CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/..")
+
+find_package(Crowdin)
+message(STATUS "Crowdin_FOUND = ${Crowdin_FOUND}")
+message(STATUS "Crowdin_EXECUTABLE = ${Crowdin_EXECUTABLE}")
--- tests/TestFindSphinx.cmake
+++ tests/TestFindSphinx.cmake
@@ -1,24 +1,26 @@
 
+cmake_minimum_required(VERSION 3.23)
 
 message(STATUS "")
 message(STATUS "---------- TestFindSphinx ----------")
 message(STATUS "")
 
 set(CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/..")
-find_package(Sphinx QUIET)
-message(STATUS "Sphinx_FOUND                  = ${Sphinx_FOUND}")
-message(STATUS "Sphinx_Build_FOUND            = ${Sphinx_Build_FOUND}")
-message(STATUS "Sphinx_Apidoc_FOUND           = ${Sphinx_Apidoc_FOUND}")
-message(STATUS "Sphinx_Autogen_FOUND          = ${Sphinx_Autogen_FOUND}")
-message(STATUS "Sphinx_Quickstart_FOUND       = ${Sphinx_Quickstart_FOUND}")
-message(STATUS "Sphinx_BUILD_EXECUTABLE       = ${Sphinx_BUILD_EXECUTABLE}")
-message(STATUS "Sphinx_APIDOC_EXECUTABLE      = ${Sphinx_APIDOC_EXECUTABLE}")
-message(STATUS "Sphinx_AUTOGEN_EXECUTABLE     = ${Sphinx_AUTOGEN_EXECUTABLE}")
-message(STATUS "Sphinx_QUICKSTART_EXECUTABLE  = ${Sphinx_QUICKSTART_EXECUTABLE}")
+
+
+# find_package(Sphinx)
+# message(STATUS "Sphinx_FOUND                  = ${Sphinx_FOUND}")
+# message(STATUS "Sphinx_Build_FOUND            = ${Sphinx_Build_FOUND}")
+# message(STATUS "Sphinx_Apidoc_FOUND           = ${Sphinx_Apidoc_FOUND}")
+# message(STATUS "Sphinx_Autogen_FOUND          = ${Sphinx_Autogen_FOUND}")
+# message(STATUS "Sphinx_Quickstart_FOUND       = ${Sphinx_Quickstart_FOUND}")
+# message(STATUS "Sphinx_BUILD_EXECUTABLE       = ${Sphinx_BUILD_EXECUTABLE}")
+# message(STATUS "Sphinx_APIDOC_EXECUTABLE      = ${Sphinx_APIDOC_EXECUTABLE}")
+# message(STATUS "Sphinx_AUTOGEN_EXECUTABLE     = ${Sphinx_AUTOGEN_EXECUTABLE}")
+# message(STATUS "Sphinx_QUICKSTART_EXECUTABLE  = ${Sphinx_QUICKSTART_EXECUTABLE}")
 
 
 find_package(Python)
-# message(STATUS "Python_EXECUTABLE = ${Python_EXECUTABLE}")
 execute_process(
     COMMAND ${Python_EXECUTABLE} -m venv .venv
     WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR})
@@ -26,7 +28,10 @@ unset(Python_EXECUTABLE)
 set(Python_ROOT_DIR ${CMAKE_CURRENT_BINARY_DIR}/.venv)
 find_package(Python)
 execute_process(
-    COMMAND ${Python_EXECUTABLE} -m pip install sphinx)
+    COMMAND ${Python_EXECUTABLE} -m pip install sphinx
+    RESULT_VARIABLE RES_VAR
+    OUTPUT_VARIABLE OUT_VAR
+    ERROR_VARIABLE  ERR_VAR)
 # message(STATUS "Python_EXECUTABLE = ${Python_EXECUTABLE}")
 
 unset(Sphinx_FOUND CACHE)
@@ -49,3 +54,17 @@ message(STATUS "Sphinx_BUILD_EXECUTABLE       = ${Sphinx_BUILD_EXECUTABLE}")
 message(STATUS "Sphinx_APIDOC_EXECUTABLE      = ${Sphinx_APIDOC_EXECUTABLE}")
 message(STATUS "Sphinx_AUTOGEN_EXECUTABLE     = ${Sphinx_AUTOGEN_EXECUTABLE}")
 message(STATUS "Sphinx_QUICKSTART_EXECUTABLE  = ${Sphinx_QUICKSTART_EXECUTABLE}")
+
+string(FIND "${Sphinx_BUILD_EXECUTABLE}" "${Sphinx_ROOT_DIR}" FOUND_INDEX)
+if(NOT FOUND_INDEX EQUAL -1)
+    message(STATUS "Found ${Sphinx_ROOT_DIR} in ${Sphinx_BUILD_EXECUTABLE}")
+else()
+    message(STATUS "${Sphinx_ROOT_DIR} not found in ${Sphinx_BUILD_EXECUTABLE}")
+endif()
+
+string(FIND "${Sphinx_APIDOC_EXECUTABLE}" "${Sphinx_ROOT_DIR}" FOUND_INDEX)
+if(NOT FOUND_INDEX EQUAL -1)
+    message(STATUS "Found ${Sphinx_ROOT_DIR} in ${Sphinx_APIDOC_EXECUTABLE}")
+else()
+    message(STATUS "${Sphinx_ROOT_DIR} not found in ${Sphinx_APIDOC_EXECUTABLE}")
+endif()
